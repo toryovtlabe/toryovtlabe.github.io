907254c64b162f99ece888ba0cead0cebc1eb1da
toryovtlabe <119455478+toryovtlabe@users.noreply.github.com>
2023-05-01 13:18:16 +0300

    without xml

--- contact_form.js
+++ contact_form.js
@@ -1,14 +1,19 @@
 let snackbar = document.getElementById("formAlert");
-document.getElementById('send').addEventListener("click", showSnackbar)
+document.getElementById('formButton').addEventListener("click", showSnackbar)
 
 function showSnackbar() {
-    snackbar.className = 'show';
     let Form = document.forms[0];
     let Phone = Form.elements.telnumber;
     let Name = Form.elements.username;
-    document.getElementById('user').innerHTML=Name.value;
-    document.getElementById('userPhone').innerHTML=Phone.value;
-    setTimeout(hideSnackbar, 2950);
+    if(Phone.value!=0 & Name.value!=0) {
+        snackbar.className = 'show';
+        document.getElementById('userName').innerHTML=Name.value;
+        document.getElementById('userPhone').innerHTML=Phone.value;
+        setTimeout(hideSnackbar, 5400);
+}
+    else{
+        alert('Уважаемый пользователь, пожалуйста заполните поля "ФИО" и "Телефон" для отправления заявления..');
+    }
 }
 
 function hideSnackbar() {
